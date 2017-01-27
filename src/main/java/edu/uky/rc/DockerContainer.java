b6fea0ac354ec8b66ee5c4cb3bc25b52cba1d765
Nathan A. McCarty <nathan@mccarty.io>
2017-01-27 13:04:45 -0500

    DockerContiner functionality
    Added functionality to DockerContainer class
    Class now has listContainers, listAllContainers, and running methods

--- src/main/java/edu/uky/rc/DockerContainer.java
+++ src/main/java/edu/uky/rc/DockerContainer.java
@@ -1,6 +1,12 @@
 package edu.uky.rc;
 
 import com.spotify.docker.client.*;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.Container;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by nmccarty on 1/27/17.
  */
@@ -16,5 +22,48 @@ public class DockerContainer {
     public String getContainerID() {
         return containerID;
     }
+    public boolean running(){
+        try {
+            return docker.inspectContainer(containerID).state().running();
+        } catch (DockerException|InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
+    public static List<DockerContainer> listContainers(){
+        List<Container> containers;
+        try{
+            containers = docker.listContainers();
+        } catch (DockerException|InterruptedException ex){
+            // TODO: Actually do something here, potentially retry or pass the error up
+            ex.printStackTrace();
+            containers = new ArrayList<>();
+        }
+
+        ArrayList<DockerContainer> dockerContainers = new ArrayList<>();
+        for(Container c : containers){
+            dockerContainers.add(new DockerContainer(c.id()));
+        }
+
+        return dockerContainers;
+    }
+
+    public static List<DockerContainer> listAllContainers(){
+        List<Container> containers;
+        try{
+            containers = docker.listContainers(DockerClient.ListContainersParam.allContainers());
+        } catch (DockerException|InterruptedException ex){
+            // TODO: Actually do something here, potentially retry or pass the error up
+            ex.printStackTrace();
+            containers = new ArrayList<>();
+        }
+
+        ArrayList<DockerContainer> dockerContainers = new ArrayList<>();
+        for(Container c : containers){
+            dockerContainers.add(new DockerContainer(c.id()));
+        }
+
+        return dockerContainers;
+    }
 }

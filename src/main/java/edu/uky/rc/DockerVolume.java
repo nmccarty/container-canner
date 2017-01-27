0e954e400b00741d08dfadd14e58827cafa4a7c8
Nathan A. McCarty <nathan@mccarty.io>
2017-01-27 13:37:42 -0500

    Add variables to DockerVolume

--- src/main/java/edu/uky/rc/DockerVolume.java
+++ src/main/java/edu/uky/rc/DockerVolume.java
@@ -4,4 +4,20 @@ package edu.uky.rc;
  * Created by nmccarty on 1/27/17.
  */
 public class DockerVolume {
+    private DockerContainer container;
+    private String internalPath;
+
+    public DockerVolume(DockerContainer c, String ip){
+        container = c;
+        internalPath = ip;
+    }
+
+    public DockerContainer getContainer() {
+        return container;
+    }
+
+    public String getInternalPath() {
+        return internalPath;
+    }
+
 }

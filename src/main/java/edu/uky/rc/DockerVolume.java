0d6ab2becc1a1f20cf503968fc9c5ff552212d45
Nathan A. McCarty <nathan@mccarty.io>
2017-02-22 17:21:49 -0500

    Add logging to saveVolume

--- src/main/java/edu/uky/rc/DockerVolume.java
+++ src/main/java/edu/uky/rc/DockerVolume.java
@@ -3,10 +3,10 @@ package edu.uky.rc;
 import com.google.common.collect.ImmutableList;
 import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.ContainerMount;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
+import java.io.*;
 
 /**
  * Created by nmccarty on 1/27/17.
@@ -47,17 +47,34 @@ public class DockerVolume {
 
     public File saveVolume(){
         // Create a temporary file to store the TARchive in
+        Logger logger = LoggerFactory.getLogger(DockerVolume.class);
         try {
+
+
+
             File temp = File.createTempFile("docker-volume"+internalPath, ".tar");
             // TODO: Use jTar to remove platform dependencies
             String command = "tar -cf " + temp.getAbsolutePath() + " -C " + getExternalPath() + " .";
+            logger.info(command);
+
+            StringBuffer output = new StringBuffer();
 
             Process p = Runtime.getRuntime().exec(command);
             p.waitFor();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String line = "";
+            while((line = reader.readLine()) != null){
+                output.append(line + "\n");
+            }
+
+            logger.info(output.toString());
+
 
             return temp;
         } catch (IOException|InterruptedException e) {
             // TODO: Error recovery?
+            logger.error("Saving volume state failed, for whatever reason.",e);
             e.printStackTrace();
             return null;
         }

1950cabf43713c2fbb2926ce50828240b70f192b
Nathan A. McCarty <nathan@mccarty.io>
2017-02-23 17:57:15 -0500

    Add constructor to CannedContainer

--- src/main/java/edu/uky/rc/CannedContainer.java
+++ src/main/java/edu/uky/rc/CannedContainer.java
@@ -1,7 +1,19 @@
 package edu.uky.rc;
 
+import com.spotify.docker.client.messages.Container;
+import com.spotify.docker.client.messages.ContainerChange;
+
+import java.util.HashMap;
+
 /**
  * Created by nmccarty on 2/23/17.
  */
 public class CannedContainer {
+    private DockerContainer container;
+    private HashMap<String,String> volumeMap;
+
+    public CannedContainer(String id){
+        container = new DockerContainer(id);
+        volumeMap = new HashMap<>();
+    }
 }

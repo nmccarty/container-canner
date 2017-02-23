eefff2b05440a69c817058038a44eb9b9eb6e772
Nathan A. McCarty <nathan@mccarty.io>
2017-02-23 18:24:26 -0500

    Add can method

--- src/main/java/edu/uky/rc/CannedContainer.java
+++ src/main/java/edu/uky/rc/CannedContainer.java
@@ -3,12 +3,14 @@ package edu.uky.rc;
 import com.spotify.docker.client.messages.Container;
 import com.spotify.docker.client.messages.ContainerChange;
 
+import java.io.File;
 import java.util.HashMap;
 
 /**
  * Created by nmccarty on 2/23/17.
  */
 public class CannedContainer {
+    // TODO: make container optional to facilitate uncanning
     private DockerContainer container;
     private HashMap<String,String> volumeMap;
 
@@ -16,4 +18,10 @@ public class CannedContainer {
         container = new DockerContainer(id);
         volumeMap = new HashMap<>();
     }
+
+    public File can(){
+
+        throw new UnsupportedOperationException();
+    }
+
 }
